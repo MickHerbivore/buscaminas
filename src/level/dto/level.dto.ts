c2e7329888feecd1e57b4d21f0e6df3f1d0e6473
Miguel Muñoz Caro <miguelmc.dev@gmail.com>
2024-12-29 19:39:22 -0300

    fix: find levels

--- src/level/dto/level.dto.ts
+++ src/level/dto/level.dto.ts
@@ -1,8 +1,16 @@
+import { Expose } from 'class-transformer';
+
 export class LevelDto {
+  @Expose()
   readonly id: string;
+  @Expose()
   readonly name: string;
+  @Expose()
   readonly description: string;
+  @Expose()
   readonly rowsQuantity: number;
+  @Expose()
   readonly columnsQuantity: number;
+  @Expose()
   readonly minesQuantity: number;
 }
